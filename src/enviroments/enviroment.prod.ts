d9c5582140519929423eeaa0dc4374cca3abfcf2
Joana Letícia <joanaleticia1973@gmail.com>
2025-05-31 21:18:05 -0300

    Enviroments

---
+++ src/enviroments/enviroment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://app.minhaempresa.com.br' // URL da API em produção
+};
---
+++ src/enviroments/enviroment.test.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://homolog.frontend.com' // URL da API de homologação
+};
